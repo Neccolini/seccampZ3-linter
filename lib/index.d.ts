659075e7718d34fbfa0d55bfd8513302ecf3c703
Neccolini <shun11202991@gmail.com>
2020-11-21 16:38:00 +0900

    added lib

---
+++ lib/index.d.ts
@@ -0,0 +1,7 @@
+declare const _default: {
+    rules: {
+        "no-literal": import("eslint").Rule.RuleModule;
+    };
+};
+export = _default;
+//# sourceMappingURL=index.d.ts.map
---
+++ lib/rules/no-literal.d.ts
@@ -0,0 +1,4 @@
+import { Rule } from "eslint";
+declare const rule: Rule.RuleModule;
+export = rule;
+//# sourceMappingURL=no-literal.d.ts.map
